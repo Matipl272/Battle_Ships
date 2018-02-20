364bba2adee691d839289d77c83a0200a696ae72
Matiks <kaszotaxxx@gmail.com>
2018-02-20 18:02:25 +0000

    Update 0.1

---
+++ Battle_Ships/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+
+
+namespace Battle_Ships
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        #region Variables
+
+
+        #endregion
+
+        
+        
+        #region PriavteMembers;
+
+        //Holds the current result of cell in game
+        private MarkType[] mResults;
+
+
+        //True if Player 1 Turn
+        private bool mPlayerTurn;
+
+        //True if game ended
+        private bool mGameEnd;
+
+
+        #endregion
+
+        #region Constructor
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            NewGame();
+        }
+
+        #endregion
+
+
+
+        private void NewGame()
+        {
+            //Create a new array for cells
+            mResults = new MarkType[100];
+
+            for (var i = 0; i < mResults.Length; i++)
+                mResults[i] = MarkType.Free;
+
+
+            //Make sure that 1st player is player 1
+            mPlayerTurn = true;
+
+        }
+
+
+    }
+}
---
+++ Battle_Ships/MarkType.cs
@@ -0,0 +1,19 @@
+﻿namespace Battle_Ships
+{
+    //Type of value A cell currently is
+
+    public enum MarkType
+    {
+        //The cell wasn't clicked
+        Free,
+
+        //The cell = Occupied by ship
+        OccupiedByShip,
+
+        //The cell = Miss
+        Miss,
+
+        //The Hit
+        Hit
+    }
+}
